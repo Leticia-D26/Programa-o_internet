63f4ffa3bfe63eff1cd14050258713c855bb77c9
Leticia-D26 <duarte.leticia@escolar.ifrn.edu.br>
2025-10-21 08:14:19 -0300

    Update tarefaController.php

--- todo_list_mvc/app/controllers/tarefaController.php
+++ todo_list_mvc/app/controllers/tarefaController.php
@@ -1,37 +1,30 @@
-<?php 
-require_once __DIR__ .'/../models/tarefa.php';
-class TarefaController{
-    private $tarefaModel;
+<?php
+require_once __DIR__ . '/../models/tarefa.php';
+class TarefaController {
+    private $tarefaModel; 
+
     public function __construct(){
-        $this->tarefaModel = new Tarefa();
-
+        $this->tarefaModel = new Tarefa(); 
     }
 
-    ## listar
     public function index(){
-        $tarefa = $this->tarefaModel->listar();
-        include __DIR__ . '/../views/listar.php';
+        $tarefas = $this->tarefaModel->listar(); 
+        include __DIR__ . '/../views/listar.php'; 
     }
 
-    ## criar
     public function criar(){
         if(isset($_POST['descricao']) && !empty(trim($_POST['descricao']))){
-            $this->tarefaModel->criar($_POST['descricao']);
+            $this->tarefaModel->criar($_POST['descricao']); 
         }
-
-        header("location: index.php");
-        
+        header("Location: index.php"); 
     }
-    # excluir
 
     public function excluir(){
         if(isset($_GET['delete'])){
-            $this->tarefaModel->excluir($_GET['delete']);
+            $this->tarefaModel->excluir($_GET['delete']); 
         }
-        header("location: index.php");
+        header("Location: index.php"); 
     }
-
 }
-
 
 ?>
