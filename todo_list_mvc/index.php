e013201b2db99b97d0c93401c6f59f28bb508616
Leticia-D26 <duarte.leticia@escolar.ifrn.edu.br>
2025-10-23 11:54:36 -0300

    prova finalizada

--- todo_list_mvc/index.php
+++ todo_list_mvc/index.php
@@ -1,17 +1,26 @@
-<?php 
-require_once __DIR__ .'/app/controllers/tarefaController.php';
+<?php
+require_once __DIR__ . '/app/controllers/TarefaController.php';
 
-$controller = new TarefaController();
+$controller = new TarefaController(); 
 
-$action = $_GET['action'] ?? 'index';
+$action = $_GET['action'] ?? 'index' ; 
 
 switch ($action) {
     case 'criar':
         $controller->criar();
+        break;
     case 'excluir':
         $controller->excluir();
         break;
+    case 'editar':
+        $controller->editar();
+        break;
+    case 'atualizar':
+        $controller->atualizar();
+        break;
     default:
-        $controller->index();            
+        $controller->index();
+        break;
 }
+
 ?>
